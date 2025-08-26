c10908a342e82dffe4a867e05f9df3695eaa80a8
flynnbm <flynn.m.brian@gmail.com>
2025-08-26 12:33:24 -0400

    create get_point_cloud service state

---
+++ compare_flexbe_states/compare_flexbe_states/get_point_cloud_service_state.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 Christopher Newport University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from flexbe_core import EventState, Logger
+
+# Adjust to your actual package name:
+from compare_flexbe_utilities.srv import GetPointCloud as SrvType # request: input, camera_pose, cluster_tolerance, min/max size
+
+class GetPointCloudServiceState(EventState):
+    """
+    Calls /get_point_cloud to collect current view point cloud and camera pose.
+
+    -- service_timeout     float    Timeout for service discovery (sec, default: 5.0)
+    -- camera_topic        str      Camera Topic (default: '/rgbd_camera/points')
+    -- target_frame        str      Target Frame (default: '/base_link')
+
+    <# camera_pose                geometry_msgs/PoseStamped     pose of camera_frame in target_frame
+    <# cloud_frame                string                       original message frame_id
+
+    <= done
+    <= failed
+    """
+    def __init__(self, service_timeout=5.0, service_name='/get_point_cloud', camera_topic='/rgbd_camera/points', target_frame='/base_link'):
+        super().__init__(outcomes=['done', 'failed'],
+                            output_keys=['cloud_out', 'camera_pose', 'cloud_frame']
+        )
+        self._camera_topic = camera_topic
+        self._target_frame = target_frame
+        self._service_timeout = service_timeout
+        self._service_name = service_name
+        self._client = None
+        self._future = None
+
+    def execute(self, userdata):
+        # Execute this method periodically while the state is active.
+        # Main purpose is to check state conditions and trigger a corresponding outcome.
+        # If no outcome is returned, the state will stay active.
+
+        if self._future is None:
+            return 'failed'
+
+        if self._future.done():
+            try:
+                result = self._future.result()
+                userdata.cloud_out = result.cloud_out
+                userdata.camera_pose = result.camera_pose
+                userdata.cloud_frame = result.cloud_frame
+                Logger.loginfo(f"[{type(self).__name__}] Received result with {result.success}.")
+                Logger.loginfo(f"[{type(self).__name__}] Received message: {result.message}.")
+                return 'done'
+            except Exception as e:
+                Logger.logerr(f"Service call failed: {str(e)}")
+                return 'failed'
+
+        return None  # still waiting
+    
+    def on_enter(self, userdata):
+        # Call this method a single time when the state becomes active, when a transition from another state to this one is taken.
+        # It is primarily used to start actions which are associated with this state.
+        
+        # construct request
+        request = SrvType.Request()
+        request.input = self._camera_topic
+        request.input = self._target_frame
+        request.input = self._service_timeout
+
+        # send request
+        try:
+            self._future = self._client.call_async(request)
+            Logger.loginfo(f"[{type(self).__name__}] Sent request to {self._service_name} service.")
+        except Exception as e:
+            Logger.logerr(f"[{type(self).__name__}] Failed to send request: {str(e)}")
+
+    def on_exit(self, userdata):
+        # Call this method when an outcome is returned and another state gets active.
+        # It can be used to stop possibly running processes started by on_enter.
+
+        # No-op: template hook
+        pass
+
+    def on_start(self):
+        # Call this method when the behavior is instantiated on board.
+        # If possible, it is generally better to initialize used resources in the constructor
+        #   because if anything failed, the behavior would not even be started.
+
+        # create the service client, andensure that the service server is initialized
+        self._client = type(self).create_client(SrvType, self._service_name)
+        if not self._client.wait_for_service(timeout_sec=self._service_timeout):
+            Logger.logerr(f"[{type(self).__name__}] Service {self._service_name} not available after waiting.")
+            return 'failed'
+
+    def on_stop(self):
+        # Call this method whenever the behavior stops execution, also if it is cancelled.
+        # Use this event to clean up things like claimed resources.
+
+        # make sure the client is destroyed when the behavior ends so it can restart cleanly
+        if self._client:
+            try:
+                self._client.destroy()
+            except Exception:
+                pass
